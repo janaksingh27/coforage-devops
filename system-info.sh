553dfc346c3d47da44ebaaa2fbf441838d4922f8
Janak Singh <janaksinghhimt@gmail.com>
2026-07-13 03:40:50 +0000

    Initial Commit

---
+++ system-info.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Hostname:"
+hostname
+
+echo
+
+echo "Current User:"
+whoami
+
+echo
+
+echo "Currnet Directory:"
+pwd
+
+echo
+
+echo "Disk usage:"
+df -h
+
+echo
+
+echo "Memory usage:"
+free -h
